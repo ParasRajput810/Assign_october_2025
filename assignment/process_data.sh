3fc4afe290ffcb5a1a854bcf611762ccc60ec15c
Paras Rajput <paras.r@smarter.codes>
2025-12-12 13:47:28 +0530

    Added the conversion and logic processed_outputs.tar.gz

--- assignment/process_data.sh
+++ assignment/process_data.sh
@@ -11,3 +11,32 @@ echo "Found archive: solver_output.tar.gz"
 tar -xzf solver_output.tar.gz
 
 echo "Extraction done"
+
+
+echo "Renaming extracted file"
+original_solver_file=$(tar -tf solver_output.tar.gz)
+tar -xzf solver_output.tar.gz "$original_solver_file"
+
+mv "$original_solver_file" solver_output.txt
+
+echo "Renamed to solver_output.txt"
+echo "Cleaning the data..."
+
+grep -E '^[[:space:]]*[0-9]+' solver_output.txt \
+  | sed -E 's/[[:space:]]*\|[[:space:]]*/,/g; s/^[[:space:]]+//; s/[[:space:]]+$//' \
+  > solver_data.csv
+
+echo "CSV file created: solver_data.csv"
+
+echo "Making csv from txt..."
+
+grep -E '^[[:space:]]*[0-9]+' solver_output.txt \
+  | sed -E 's/[[:space:]]*\|[[:space:]]*/,/g; s/^[[:space:]]+//; s/[[:space:]]+$//' \
+  > solver_data.csv
+
+echo "Packing files into tar..."
+
+tar -czf processed_outputs.tar.gz solver_output.txt solver_data.csv
+
+echo "Done with processed_outputs.tar.gz"
+echo "Cleaning is completed!"
